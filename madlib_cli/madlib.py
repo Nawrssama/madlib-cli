daafbcaee6dc2831489793b556e50db7d542e44c
Nawrssama <nawrsbibi.nb@gmail.com>
2023-04-11 09:15:38 +0000

    added docstring and fixed the code to fit the example

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,22 +1,77 @@
 import re
 
 def read_template(path):
-    if path != "assets/sekiro_game.txt" :
-        if path != "assets/great_and_amazing_game.txt" :
+
+    """
+    Reads a file from the specified path and returns its contents as a string.
+
+    Parameters:
+    - path (str): The path to the file to be read.
+
+    Raises:
+    - FileNotFoundError: If the specified path does not exist or is not a valid file.
+
+    Returns:
+    - str: The contents of the file as a string.
+    """
+
+    if path != "assets/text_befor.txt" :
+        if path != "assets/dark_and_stormy_night_template.txt" :
             raise FileNotFoundError ("path does not exsist")
     with open(path) as file:
         return file.read()
 
 def parse_template(str):
+
+    """
+    Parses a template string and returns a tuple containing the stripped template and its variable parts.
+
+    Parameters:
+    - string (str): The template string to be parsed.
+
+    Returns:
+    - tuple (str, tuple): A tuple containing the stripped template and its variable parts.
+        - The stripped template (str) is the original template string with its variable parts replaced by '{}' placeholders.
+        - The variable parts (tuple) is a tuple of strings representing the variable parts of the original template string, in the order they appear.
+    """
+        
     parts = re.findall(r'\{([^{}]+)\}', str)
     stripped_template = re.sub(r'\{([^{}]+)\}', '{}', str)
     stripped_parts = [part.strip() for part in parts]
     return stripped_template, tuple(stripped_parts)
 
 def merge(stripped_template, user_inputs):
+
+    """
+    Merges user inputs with a stripped template string and returns the merged string.
+
+    Parameters:
+    - stripped_template (str): The stripped template string to merge user inputs with.
+    - user_inputs (tuple): A tuple of strings representing the user inputs to be merged with the stripped template string.
+
+    Returns:
+    - str: The merged string, with the user inputs inserted into the stripped template string using the '{}' placeholders.
+    """
+
     return stripped_template.format(*user_inputs)
 
 def play_game(template_path):
+
+    """
+    Plays a madlibs game using the specified template file.
+
+    Parameters:
+    - template_path (str): The path to the template file to use for the madlibs game.
+
+    Returns:
+    - None
+
+    Side Effects:
+    - Prompts the user to enter values for the variable parts in the template.
+    - Prints the resulting madlibs story to the console.
+    - Writes the resulting madlibs story to a file named "assets/text_after.txt".
+    """
+
     template = read_template(template_path)
     stripped_template, parts = parse_template(template)
 
@@ -28,14 +83,16 @@ def play_game(template_path):
     madlib = merge(stripped_template, user_inputs)
     print(madlib)
 
-    with open("assets/complete_sekiro_game.txt", "w") as file:
+    with open("assets/text_after.txt", "w") as file:
         file.write(madlib)
 
 if __name__ == "__main__":
+    # This block of code is executed when the Python script is run as the main program.
+    # It welcomes the user to the madlib game and calls the play_game function with the template path set to "assets/text_befor.txt".
     print('''
 ******** Welcome to the madlib game ********
 ***** in this game you can play by entering what you are asked ;) *****
 ''')
 
-    template_path = "assets/sekiro_game.txt"
+    template_path = "assets/text_befor.txt"
     play_game(template_path)
--- tests/test_madlib.py
+++ tests/test_madlib.py
@@ -1,10 +1,9 @@
 import pytest
-
 from madlib_cli.madlib import read_template, parse_template, merge
 
 
 def test_read_template_returns_stripped_string():
-    actual = read_template("assets/great_and_amazing_game.txt")
+    actual = read_template("assets/dark_and_stormy_night_template.txt")
     expected = "It was a {Adjective} and {Adjective} {Noun}."
     assert actual == expected
 
